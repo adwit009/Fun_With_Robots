b5973650951074c91d4d6985c83ce6e373ad5322
adwit009 <vasu6895@gmail.com>
2017-06-27 11:55:47 -0700

    Add files via upload

---
+++ src/INITIAL.java
@@ -0,0 +1,39 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class INITIAL {
+public static void main(String[] args) {
+	
+	Robot bob = new Robot("batman");
+
+	bob.setSpeed(200);
+	bob.turn(90);
+	bob.move(-300);
+	bob.turn(-90);
+	bob.turn(30);
+	bob.penDown();
+	bob.move(205);
+	bob.turn(130);
+	bob.move(200);
+	bob.turn(180);
+	bob.move(100);
+	bob.turn(-70);
+	bob.move(84);
+	bob.penUp();
+bob.move(-150);
+bob.turn(-120);
+bob.move(100);
+bob.turn(-150);
+	bob.turn(30);
+bob.penDown();
+bob.move(205);
+bob.turn(130);
+bob.move(200);
+bob.turn(180);
+bob.move(100);
+bob.turn(-70);
+bob.move(84);
+bob.penUp();
+bob.move(-150);
+
+}
+}
