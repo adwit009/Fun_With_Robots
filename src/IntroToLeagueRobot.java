06e0880fb4984a071206c4a8ad028eb104393db6
adwit009 <vasu6895@gmail.com>
2017-06-27 11:15:09 -0700

    Add files via upload

--- src/IntroToLeagueRobot.java
+++ src/IntroToLeagueRobot.java
@@ -3,5 +3,12 @@ import org.jointheleague.graphical.robot.Robot;
 public class IntroToLeagueRobot {
 	public static void main(String[] args) {
 		//put your code here
+	Robot bob = new Robot();
+	bob.move(10);
+	bob.sparkle();
+	
+	
+	
+	
 	}
 }
